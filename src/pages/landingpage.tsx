b3af0dd299b3ac65496377652871df4b56e3a4f3
Jake TyCyn <jaketycyn@gmail.com>
2023-03-27 17:58:08 -0700

    clean up - Files

--- src/pages/landingpage.tsx
+++ src/pages/landingpage.tsx
@@ -22,29 +22,6 @@ const Landingpage = (props) => {
   const [isLoading, setLoading] = useState(true);
   return (
     <div>
-      <div className="grid grid-cols-3">
-        {/* {gameData.map((game, index) => {
-          return (
-            <div key={game.id}>
-              <Image
-                src={game.image}
-                alt={game.name}
-                width="240"
-                height="240"
-                style={{ objectFit: "contain" }}
-                className={cn(
-                  "h-auto w-full duration-700 ease-in-out group-hover:scale-110",
-                  isLoading
-                    ? "scale-110 blur-2xl grayscale"
-                    : "scale-100 blur-0 grayscale-0"
-                )}
-                onLoadingComplete={() => setLoading(false)}
-              />
-            </div>
-          );
-        })}
-      */}
-      </div>
       <Link href={"/sanctum/question/what-are-your-league-plans"}>
         <button className="h-full w-full rounded-lg bg-zinc-900 p-4 text-2xl  font-semibold text-neutral-200 hover:bg-zinc-700">
           Help me find a leaguestarter
